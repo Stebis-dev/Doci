08d18576b660847f7ca9d518251382c2e26602f7
Gediminas Šeibokas <65848857+Stebis-dev@users.noreply.github.com>
2025-05-09 19:29:46 +0300

    fix CI/CD

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,7 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async (): Promise<Config> => ({
   projects: await getJestProjectsAsync(),
+  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }], 'text-summary'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
 });
